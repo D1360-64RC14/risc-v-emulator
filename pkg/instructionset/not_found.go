1fcd804fdb4034dc98682113f058a02de42c8d7a
D1360-64RC14 <d1360_64rc14@protonmail.com>
2024-03-10 20:26:48 -0300

    fix: ignore mem arg from NotFound inst

--- pkg/instructionset/not_found.go
+++ pkg/instructionset/not_found.go
@@ -7,6 +7,6 @@ import (
 )
 
 // NotFound identifies an undefined instruction.
-func NotFound(_ *[32]uint32, pc *uint32, mem interfaces.Memory[uint32], inst uint32) {
+func NotFound(_ *[32]uint32, pc *uint32, _ interfaces.Memory[uint32], inst uint32) {
 	fmt.Printf("at %X: not found inst %b\n", *pc, inst)
 }
